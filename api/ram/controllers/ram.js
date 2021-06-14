f236add7f7bcdb533b444113cc86d88e8ea60859
BuildTools <unconfigured@null.spigotmc.org>
2021-06-14 20:55:39 +0800

    crud edit update

--- api/ram/controllers/ram.js
+++ api/ram/controllers/ram.js
@@ -61,5 +61,49 @@ module.exports = {
     }
   },
   crud_editPage: async(ctx) => {
+    const id = ctx.params.id;
+    try {
+      // const [rows] = await db.query('SELECT * FROM books WHERE id = ?', [id]);
+      const response = await fetch(`http://localhost:1337/rams/${id}`);
+      const data = await response.json();
+      return await ctx.render('crud_ram/edit', {
+        id: data.id,
+        name: data.name,
+        clock_rate: data.clock_rate,
+        size: data.size,
+        image_url: data.image_url,
+        ram_ID: data.ram_ID
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  crud_update: async(ctx) => {
+    const id = ctx.request.body.id;
+    const name = ctx.request.body.name;
+    const clock_rate = ctx.request.body.clock_rate;
+    const size = ctx.request.body.size;
+    const image_url = ctx.request.body.image_url;
+    const ram_ID = ctx.request.body.ram_ID;
+    console.log(name, clock_rate, size, image_url, ram_ID,id);
+  
+    const form_data = {
+      name,
+      clock_rate,
+      size, 
+      image_url, 
+      ram_ID,
+    };
+    try {
+      const response = await fetch(`http://localhost:1337/rams/${id}`, {
+        method: 'put',
+        body: JSON.stringify(form_data), 
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const data = await response.json();
+      return await ctx.redirect('/crud_ram');
+    } catch (err) {
+      console.log(err);
+    }
   },
 };
