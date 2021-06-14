cd901024fb1bd58c09a6c0de5f589e2bce4b9a5c
BuildTools <unconfigured@null.spigotmc.org>
2021-06-14 20:45:24 +0800

    crud add

--- api/ram/controllers/ram.js
+++ api/ram/controllers/ram.js
@@ -14,4 +14,52 @@ module.exports = {
       return await ctx.render('crud_ram/index', { data: '' });
     }
   },
+  crud_addPage: async(ctx) => {
+      
+      return await ctx.render('crud_ram/add', {
+        name: '',
+        clock_rate: '',
+        size: '',
+        image_url: '',
+        ram_ID: '',
+      });
+  },
+  crud_add: async(ctx) => {
+    const name = ctx.request.body.name;
+    const clock_rate = ctx.request.body.clock_rate;
+    const size = ctx.request.body.size;
+    const image_url = ctx.request.body.image_url;
+    const ram_ID = ctx.request.body.ram_ID;
+    console.log(name, clock_rate, size, image_url, ram_ID);
+
+    const form_data = {
+      name,
+      clock_rate,
+      size, 
+      image_url, 
+      ram_ID,
+    };
+
+    try {
+      // await db.query('INSERT INTO books SET ?', form_data);
+      const response = await fetch('http://localhost:1337/rams', {
+        method: 'post',
+        body: JSON.stringify(form_data), 
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const data = await response.json();
+      return await ctx.redirect('/crud_ram');
+    } catch (err) {
+      console.log(err);
+      return await ctx.render('crud_ram/add', {
+        name: form_data.name,
+        clock_rate: form_data.clock_rate,
+        size: form_data.size,
+        image_url: form_data.image_url,
+        ram_ID: form_data.ram_ID
+      });
+    }
+  },
+  crud_editPage: async(ctx) => {
+  },
 };
